38f98e129d7473dab7d4618aaebdf49f7f2c8c18
dimasazissurahman <dimasazissurahman@gmail.com>
2022-04-12 16:43:09 +0700

    enhancement hours

--- src/pages/waiting.page.tsx
+++ src/pages/waiting.page.tsx
@@ -2,7 +2,7 @@ import { CountdownTimer } from 'components/countdown/countdown'
 import React from 'react'
 
 const WaitingPage = () => {
-  const deadline = new Date(Date.parse('13 April 2022 00:00:00 GMT'));
+  const deadline = new Date(Date.parse('12 April 2022 23:59:59 GMT+7'));
   const DEADLINE_IN_MS = deadline.getTime();
 
   const now = new Date().getTime();
